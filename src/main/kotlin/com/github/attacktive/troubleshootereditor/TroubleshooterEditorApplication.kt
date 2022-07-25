96ac43b55535074d2c114c7015ba3d8d11375861
Attacktive <welcome.hell.dude@gmail.com>
2022-07-25 17:51:04 +0900

    Initial commit

---
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplication.kt
@@ -0,0 +1,11 @@
+package com.github.attacktive.troubleshootereditor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TroubleshooterEditorApplication
+
+fun main(args: Array<String>) {
+	runApplication<TroubleshooterEditorApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.github.attacktive.troubleshootereditor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TroubleshooterEditorApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
