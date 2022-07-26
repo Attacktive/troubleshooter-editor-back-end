1c1b878ab26c1d91e17ed81191b3068d2e16c881
Attacktive <welcome.hell.dude@gmail.com>
2022-07-26 15:43:38 +0900

    Try retrieving company data
    TODO: The company properties is empty now.

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/upload/UploadController.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/upload/UploadController.kt
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-class UploadController(private val uploadService: UploadService, private val propertiesConfiguration: PropertiesConfiguration) {
+class UploadController(private val uploadService: UploadService, private val sqliteService: SqliteService) {
 	private val logger = LoggerFactory.getLogger(UploadController::class.java)
 
 	@PostMapping(value = ["/upload"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
 	fun upload(@RequestPart("file") multipartFile: MultipartFile): Any {
 		logger.info("${multipartFile.name} (size: ${multipartFile.size})")
 		val savedFileName = uploadService.saveFile(multipartFile)
+		sqliteService.run(savedFileName)
 
 		return mapOf("x" to "y")
 	}
