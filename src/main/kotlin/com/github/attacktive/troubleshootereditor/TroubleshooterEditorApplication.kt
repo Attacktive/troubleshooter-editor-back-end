9371818026824d17906a66dd7c44a12e52d20c7f
Attacktive <welcome.hell.dude@gmail.com>
2022-07-26 12:36:52 +0900

    Add configurations about CORS

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplication.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplication.kt
@@ -1,9 +1,12 @@
 package com.github.attacktive.troubleshootereditor
 
+import com.github.attacktive.troubleshootereditor.configuration.CorsPropertiesConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsPropertiesConfiguration::class)
 class TroubleshooterEditorApplication
 
 fun main(args: Array<String>) {
