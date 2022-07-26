f2f368d2da2593e55e65286f27eee379ea08b080
Attacktive <welcome.hell.dude@gmail.com>
2022-07-26 14:52:22 +0900

    Migrate CorsPropertiesConfiguration to PropertiesConfiguration in order to handle more properties

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplication.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/TroubleshooterEditorApplication.kt
@@ -1,12 +1,12 @@
 package com.github.attacktive.troubleshootereditor
 
-import com.github.attacktive.troubleshootereditor.configuration.CorsPropertiesConfiguration
+import com.github.attacktive.troubleshootereditor.configuration.PropertiesConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(CorsPropertiesConfiguration::class)
+@EnableConfigurationProperties(PropertiesConfiguration::class)
 class TroubleshooterEditorApplication
 
 fun main(args: Array<String>) {
