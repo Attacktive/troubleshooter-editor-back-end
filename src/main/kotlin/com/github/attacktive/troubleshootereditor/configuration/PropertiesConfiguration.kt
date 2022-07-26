9ade006f44e3d6db3521aaad0bc080b62c7d9aca
Attacktive <welcome.hell.dude@gmail.com>
2022-07-26 14:52:22 +0900

    Migrate CorsPropertiesConfiguration to PropertiesConfiguration in order to handle more properties

---
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/configuration/PropertiesConfiguration.kt
@@ -0,0 +1,22 @@
+package com.github.attacktive.troubleshootereditor.configuration
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+
+@ConfigurationProperties(prefix = "app")
+@ConfigurationPropertiesScan
+class PropertiesConfiguration {
+	val cors: Cors = Cors()
+	val file: File = File()
+
+	class Cors {
+		val origins: List<String> = mutableListOf()
+	}
+
+	class File {
+		var pathToUpload = ""
+			set(value) {
+				field = value
+			}
+	}
+}
