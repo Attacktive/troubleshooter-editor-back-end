119961be204c0189c9fb9c76e309ef65345d5828
Attacktive <welcome.hell.dude@gmail.com>
2022-07-27 15:02:47 +0900

    Add rosters support

---
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Identifiable.kt
@@ -0,0 +1,5 @@
+package com.github.attacktive.troubleshootereditor.model
+
+interface Identifiable<T> {
+	fun getId(): T
+}
--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/SaveData.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/SaveData.kt
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 
 data class SaveData(
 	val company: Company,
-	val quests: List<Quest>
+	val quests: List<Quest>,
+	val rosters: List<Roster>
 ) {
 	override fun toString(): String {
 		val objectMapper = ObjectMapper()
