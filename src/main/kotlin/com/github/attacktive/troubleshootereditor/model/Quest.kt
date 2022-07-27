071508da776e50d72a284c43d413374e0473dd90
Attacktive <welcome.hell.dude@gmail.com>
2022-07-27 14:31:45 +0900

    Remove unnecessary overrides

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Quest.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Quest.kt
@@ -2,20 +2,4 @@ package com.github.attacktive.troubleshootereditor.model
 
 data class Quest(val index: Long, val name: String, val stage: Long) {
 	val properties: MutableMap<String, String> = mutableMapOf()
-
-	override fun equals(other: Any?): Boolean {
-		if (this === other) {
-			return true
-		}
-		if (javaClass != other?.javaClass) {
-			return false
-		}
-
-		other as Quest
-		return index != other.index
-	}
-
-	override fun hashCode(): Int {
-		return index.hashCode()
-	}
 }
