f8bf9660fae0d6cea83f88490cd9745dcf51ebd1
Attacktive <welcome.hell.dude@gmail.com>
2022-07-28 22:43:12 +0900

    Remove redundant JSON property from Quest

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Quest.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Quest.kt
@@ -1,8 +1,11 @@
 package com.github.attacktive.troubleshootereditor.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 data class Quest(val index: Long, val name: String, val stage: Long): Identifiable<Long> {
 	val properties: MutableMap<String, String> = mutableMapOf()
 
+	@JsonIgnore
 	override fun getId(): Long {
 		return index
 	}
