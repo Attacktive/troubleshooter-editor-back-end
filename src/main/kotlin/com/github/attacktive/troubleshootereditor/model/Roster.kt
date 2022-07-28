60e1ebeb8c623b29557c2001cd29cf6137c5494d
Attacktive <welcome.hell.dude@gmail.com>
2022-07-28 22:41:33 +0900

    Refactor code

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Roster.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Roster.kt
@@ -1,6 +1,6 @@
 package com.github.attacktive.troubleshootereditor.model
 
-class Roster(val id: Long, val name: String, val `class`: String, val level: Long, val exp: Long): Identifiable<Long> {
+class Roster(private val id: Long, val name: String, val `class`: String, val level: Long, val exp: Long): Identifiable<Long> {
 	val properties: MutableMap<String, String> = mutableMapOf()
 
 	override fun getId(): Long {
