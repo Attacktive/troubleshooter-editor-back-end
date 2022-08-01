ae6c3a431e221b0b0aea89a209cca90039d079b6
Attacktive <welcome.hell.dude@gmail.com>
2022-08-01 16:24:26 +0900

    Reformat code

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/SaveData.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/model/SaveData.kt
@@ -2,12 +2,7 @@ package com.github.attacktive.troubleshootereditor.model
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
-data class SaveData(
-	val company: Company,
-	val quests: List<Quest>,
-	val rosters: List<Roster>,
-	val items: List<Item>
-) {
+data class SaveData(val company: Company, val quests: List<Quest>, val rosters: List<Roster>, val items: List<Item>) {
 	override fun toString(): String {
 		return jacksonObjectMapper().writeValueAsString(this)
 	}
