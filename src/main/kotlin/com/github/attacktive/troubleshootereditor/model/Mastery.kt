d1343a348b5b9d7314184d2d7666f5a5e6f60e19
Attacktive <welcome.hell.dude@gmail.com>
2022-12-29 15:38:47 +0900

    Remove dead code

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/model/Mastery.kt
+++
@@ -1,8 +0,0 @@
-package com.github.attacktive.troubleshootereditor.model
-
-data class Mastery(
-	// TODO: roster
-	private val name: String,
-	//private val level: Long,
-	//private val boardIndex: Long
-)
