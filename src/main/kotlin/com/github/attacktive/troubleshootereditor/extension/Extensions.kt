2eb5fd0abfb829c6520c81d4d34b7478d1e107d7
Attacktive <tribute2pantera@gmail.com>
2023-12-07 18:59:10 +0900

    refactor: replace use of `stream` by sequence
    https://github.com/Attacktive/troubleshooter-editor-back-end/issues/58

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/extension/Extensions.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/extension/Extensions.kt
@@ -2,9 +2,7 @@ package com.github.attacktive.troubleshootereditor.extension
 
 import com.github.attacktive.troubleshootereditor.model.Identifiable
 
-fun <I, T: Identifiable<I>> MutableSet<T>.findById(id: I): T? {
-	return stream()
-		.filter { it.getId() == id }
-		.findFirst()
-		.orElse(null)
+fun <I, T: Identifiable<I>> Collection<T>.findById(id: I): T? {
+	return asSequence()
+		.find { it.getId() == id }
 }
