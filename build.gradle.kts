356f77d1fc82df2e4f429253a2da3445e28ed630
Attacktive <tribute2pantera@gmail.com>
2024-01-12 17:57:18 +0900

    feature: extract identical String literals to single local variable
    https://app.codacy.com/gh/Attacktive/troubleshooter-editor-back-end/issues/131147840883

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	val kotlinPluginVersion = "1.9.22"
+
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
-	kotlin("plugin.jpa") version "1.9.22"
+	kotlin("jvm") version kotlinPluginVersion
+	kotlin("plugin.spring") version kotlinPluginVersion
+	kotlin("plugin.jpa") version kotlinPluginVersion
 }
 
 group = "com.github.attacktive"
