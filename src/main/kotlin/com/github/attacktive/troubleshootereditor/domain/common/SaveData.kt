1d22b5de46154618f2abbc7300dc7de495b98420
Attacktive <tribute2pantera@gmail.com>
2024-01-30 10:48:55 +0900

    feature: introduce functions to split items in two sorts

--- src/main/kotlin/com/github/attacktive/troubleshootereditor/domain/common/SaveData.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/domain/common/SaveData.kt
@@ -1,5 +1,6 @@
 package com.github.attacktive.troubleshootereditor.domain.common
 
+import com.fasterxml.jackson.annotation.JsonGetter
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.attacktive.troubleshootereditor.domain.company.Company
 import com.github.attacktive.troubleshootereditor.domain.item.Item
@@ -7,4 +8,10 @@ import com.github.attacktive.troubleshootereditor.domain.roster.Roster
 
 data class SaveData(val company: Company, val rosters: List<Roster>, val items: List<Item>) {
 	override fun toString(): String = jacksonObjectMapper().writeValueAsString(this)
+
+	@JsonGetter
+	fun gears() = items.filter { it.isGear() }
+
+	@JsonGetter
+	fun consumables() = items.filter { !it.isGear() }
 }
