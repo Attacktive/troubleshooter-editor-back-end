e09a57a311ffe557603a5a213dee62ec6436872b
Attacktive <tribute2pantera@gmail.com>
2024-05-05 14:51:30 +0900

    chore: upgrade Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 	val kotlinPluginVersion = "1.9.23"
 
 	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version kotlinPluginVersion
 	kotlin("plugin.spring") version kotlinPluginVersion
 	kotlin("plugin.jpa") version kotlinPluginVersion
