a204efa81e4f46405642d880cfbeb8edccc58c42
Attacktive <tribute2pantera@gmail.com>
2024-10-29 13:58:22 +0900

    chore: update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.attacktive"
-version = "1.2.4"
+version = "1.2.5"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 plugins {
