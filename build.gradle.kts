c5e0e315fdde6222e339e420e60e2e9d168300df
Attacktive <tribute2pantera@gmail.com>
2024-12-02 19:24:52 +0900

    feature: remove unnecessary log in case of HTTP 404

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.attacktive"
-version = "1.2.8"
+version = "1.2.9"
 java.sourceCompatibility = JavaVersion.VERSION_21
 
 plugins {
--- src/main/kotlin/com/github/attacktive/troubleshootereditor/common/controller/GlobalControllerExceptionHandler.kt
+++ src/main/kotlin/com/github/attacktive/troubleshootereditor/common/controller/GlobalControllerExceptionHandler.kt
@@ -2,14 +2,23 @@ package com.github.attacktive.troubleshootereditor.common.controller
 
 import com.github.attacktive.troubleshootereditor.common.extension.logger
 import com.github.attacktive.troubleshootereditor.ingamedata.common.ErrorResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageConversionException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.resource.NoResourceFoundException
 
 @RestControllerAdvice
 class GlobalControllerExceptionHandler {
 	private val logger by logger()
+
+	@ExceptionHandler(NoResourceFoundException::class)
+	fun handleNoResourceFoundException(exception: NoResourceFoundException): ResponseEntity<ErrorResponse> {
+		logger.error("Resource not found: /{}", exception.resourcePath)
+
+		return ResponseEntity(ErrorResponse(exception), HttpStatus.NOT_FOUND)
+	}
 
 	@ExceptionHandler(HttpMessageConversionException::class)
 	fun handleHttpMessageConversionException(exception: HttpMessageConversionException): ResponseEntity<ErrorResponse> {
